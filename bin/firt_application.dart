29db1ee12777d8b7df9ff40d4f75b39b2d679197
cxhood <cxhoodgoats258copany@gmail.com>
2023-01-24 10:09:55 -0200

    first commit

---
+++ bin/firt_application.dart
@@ -0,0 +1,5 @@
+import 'package:firt_application/firt_application.dart' as firt_application;
+
+void main(List<String> arguments) {
+  print('Hello world: ${firt_application.calculate()}!');
+}
---
+++ test/firt_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:firt_application/firt_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
